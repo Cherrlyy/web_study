a8bf9c7cf3b992a5ffa5356f3a1b75e3d378e6d3
Cherrlyy <ind07229@naver.com>
2021-08-01 14:48:13 +0900

    basic command

--- src/App.js
+++ src/App.js
@@ -18,12 +18,14 @@ export default App;
 <title> 제목 : toolbar 표시 / 즐겨찾기 제목 / 검색 엔진 제목</title>
 <body> 눈에 보이는 데이터 : 웹 문서에 표햔되는 정보</body>
 
+//box
 <header></header>
 <footer></footer>
 <nav></nav>
 <aside></aside>
 <main></main>
 
+//
 <a></a>
 <button></button>
 <input></input>
