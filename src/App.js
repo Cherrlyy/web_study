0783d7285afe59952c920ed3720311ebc327b8a6
Cherrlyy <ind07229@naver.com>
2021-08-08 21:34:07 +0900

    third

--- src/App.js
+++ src/App.js
@@ -1,29 +1,66 @@
-import moment from 'moment'
 import React, { Component } from 'react'
 import Header from './Header'
 import Calendar from './Calendar'
+ 
+import moment from 'moment'
+import './RCA.css'
+
+
+// 식단
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Table from 'react-bootstrap/Table'
 
 export default class App extends Component {
+    
     state = {
         calendarYM : moment(),
-        today : moment()
+        today : moment(),
+        selected : moment().format("YYYY-MM-DD")
     }
-
+ 
+    static defaultProps = {
+        clickFn : ()=>{}
+    }
+ 
     moveMonth = (month) => {
         this.setState({
             calendarYM : this.state.calendarYM.add(month,'M')
         })
     }
+ 
+    changeSelected = (clickedDate) =>{
+ 
+        
+        if(moment(clickedDate).isSame(this.state.selected,'day')){
+            this.props.clickFn(clickedDate);
+            return;
+        }
+ 
+        this.setState({
+            selected : clickedDate
+        })
+ 
+        this.props.clickFn(clickedDate)
+        
+        if(moment(clickedDate).isBefore(this.state.calendarYM,'month')){
+            this.moveMonth(-1)
+        }else if(moment(clickedDate).isAfter(this.state.calendarYM,'month')){
+            this.moveMonth(1)
+        }
+    }
     
-    render(){
+    render() {
         return (
             <div className="test-layout">
                 <div className="RCA-app-container">
-                    <Header calendarYM={ this.state.calendarYM.format("YYYY년 MM월")}
+                    <Header calendarYM={this.state.calendarYM.format("YYYY년 MM월")}
                         today={this.state.today.format("현재 YYYY - MM - DD")}
                         moveMonth={this.moveMonth}
                     />
-                    <Calendar />
+                    <Calendar YM={this.state.calendarYM.format("YYYY-MM-DD")}
+                        selected={this.state.selected}
+                        changeSelected={this.changeSelected}
+                    />
                 </div>
             </div>
         )
