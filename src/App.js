1992851bdb7e594bc4c7b037aec809ddd50e11ad
Cherrlyy <ind07229@naver.com>
2021-08-20 21:26:50 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,68 +1,41 @@
-import React, { Component } from 'react'
-import Header from './Header'
-import Calendar from './Calendar'
- 
-import moment from 'moment'
-import './RCA.css'
+import React from 'react'
 
-
-// 식단
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import Table from 'react-bootstrap/Table'
-
-export default class App extends Component {
-    
-    state = {
-        calendarYM : moment(),
-        today : moment(),
-        selected : moment().format("YYYY-MM-DD")
-    }
- 
-    static defaultProps = {
-        clickFn : ()=>{}
-    }
- 
-    moveMonth = (month) => {
-        this.setState({
-            calendarYM : this.state.calendarYM.add(month,'M')
-        })
-    }
- 
-    changeSelected = (clickedDate) =>{
- 
-        
-        if(moment(clickedDate).isSame(this.state.selected,'day')){
-            this.props.clickFn(clickedDate);
-            return;
-        }
- 
-        this.setState({
-            selected : clickedDate
-        })
- 
-        this.props.clickFn(clickedDate)
-        
-        if(moment(clickedDate).isBefore(this.state.calendarYM,'month')){
-            this.moveMonth(-1)
-        }else if(moment(clickedDate).isAfter(this.state.calendarYM,'month')){
-            this.moveMonth(1)
-        }
-    }
-    
+class App extends React.Component {
+    state = { selectedFiles: null };
+    componentDidUpdate = prevState => {
+      if (prevState.selectedFiles !== this.state.selectedFiles) {
+        this.renderPreviews();
+      }
+    };
+  
+    renderPreviews = () => {
+      const { selectedFiles } = this.state;
+      const previewContainer = document.getElementById("preview-container");
+      for (let i = 0; i < selectedFiles.length; i++) {
+        const preview = document.createElement("img");
+        preview.id = `preview_${i}`;
+        previewContainer.appendChild(preview);
+        const reader = new FileReader();
+        reader.onload = function(evt) {
+          preview.src = reader.result;
+        };
+        reader.readAsDataURL(selectedFiles[i]);
+      }
+    };
+    fileChangedHandler = event => {
+      const files = event.target.files;
+      this.setState({
+        selectedFiles: files
+      });
+    };
     render() {
-        return (
-            <div className="test-layout">
-                <div className="RCA-app-container">
-                    <Header calendarYM={this.state.calendarYM.format("YYYY년 MM월")}
-                        today={this.state.today.format("현재 YYYY - MM - DD")}
-                        moveMonth={this.moveMonth}
-                    />
-                    <Calendar YM={this.state.calendarYM.format("YYYY-MM-DD")}
-                        selected={this.state.selected}
-                        changeSelected={this.changeSelected}
-                    />
-                </div>
-            </div>
-        )
+      return (
+        <div className="App" style={{ marginTop: "100px" }}>
+          <input type="file" multiple onChange={this.fileChangedHandler} />
+          <div id="preview-container" />
+        </div>
+      );
     }
-}
+  }
+  
+  export default App;
